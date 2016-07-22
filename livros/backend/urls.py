87f897fa5c7c5d92a3e475f9e09bf43fdc353417
Guido <guidopercu@gmail.com>
2016-07-22 11:57:47 -0300

    Creating the API

--- livros/backend/urls.py
+++ livros/backend/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+# from django.conf.urls import url
+# from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+from rest_framework.routers import DefaultRouter
+from ebook.views import LivroViewSet, EbookViewSet
+
+# urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+# ]
+
+router = DefaultRouter()
+router.register(prefix='livros', viewset=LivroViewSet)
+router.register(prefix='ebooks', viewset=EbookViewSet)
+
+urlpatterns = router.urls
