890b496791435ce81651dce8d88e16a0cbb58f4a
Guido <guidopercu@gmail.com>
2016-07-22 11:34:45 -0300

    Creating ebook models

---
+++ livros/backend/ebook/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Livro(models.Model):
+    """ Classe com as informações do livro """
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024)
+    isbn = models.CharField(max_length=13)
+    publication_date = models.DateTimeField()
+
+
+class Autor(models.Model):
+    """ Classe com as informações do Autor.
+    - Um autor pode ser autor de n livros. Um livro pode ter n autores."""
+    pass
+
+
+class Ebook(models.Model):
+    """ Classe com as informações do ebook.
+    - Cada ebook pertence a um livro """
+    book = models.ForeignKey(Livro)
+    name = models.CharField(max_length=256)
+    file_format = models.CharField(max_length=16)
+    file_size = models.IntegerField(default=0)
+    base_url = models.URLField(max_length=1024)
+    url = models.URLField(max_length=1024)
+    image_url = models.URLField(max_length=1024)
