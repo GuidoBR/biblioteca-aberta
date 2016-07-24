19b233de0dd0cc1efb3b8c45ff653396d7bf6d1e
Guido <guidopercu@gmail.com>
2016-07-24 11:56:02 -0300

    Migration for Autor and Language

--- livros/backend/ebook/models.py
+++ livros/backend/ebook/models.py
@@ -2,27 +2,47 @@ from django.db import models
 from django.utils import timezone
 
 
+class Autor(models.Model):
+    """ Classe com as informações do Autor.
+    - Um autor pode ser autor de n livros. Um livro pode ter n autores."""
+    name = models.CharField(max_length=256, default="Autor Desconhecido")
+
+    def __str__(self):
+        return self.name
+
+
 class Livro(models.Model):
     """ Classe com as informações do livro """
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=1024)
     isbn = models.CharField(max_length=13)
     publication_date = models.DateTimeField(default=timezone.now)
+    book_writer = models.ManyToManyField(Autor)
+
+    def __str__(self):
+        return self.name
 
 
-class Autor(models.Model):
-    """ Classe com as informações do Autor.
-    - Um autor pode ser autor de n livros. Um livro pode ter n autores."""
-    pass
+class Language(models.Model):
+    """ Idioma """
+    name = models.CharField(max_length=256, default="Português")
+    language_code = models.CharField(max_length=5, default="pt-BR")
+
+    def __str__(self):
+        return self.name
 
 
 class Ebook(models.Model):
     """ Classe com as informações do ebook.
     - Cada ebook pertence a um livro """
     book = models.ForeignKey(Livro)
+    language = models.ForeignKey(Language)
     name = models.CharField(max_length=256)
     file_format = models.CharField(max_length=16)
     file_size = models.IntegerField(default=0)
     base_url = models.URLField(max_length=1024)
     url = models.URLField(max_length=1024)
     image_url = models.URLField(max_length=1024)
+
+    def __str__(self):
+        return self.name
