df036575aca4e6e91fbf87bd4086040dbb205ce4
Guido <guidopercu@gmail.com>
2016-07-26 11:09:45 -0300

    Creating defaultmixins

--- livros/backend/ebook/views.py
+++ livros/backend/ebook/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import authentication, permissions, viewsets
 from ebook.models import Livro, Ebook, Autor, Idioma
 from ebook.serializers import (LivroSerializer,
                                EbookSerializer,
@@ -6,25 +6,42 @@ from ebook.serializers import (LivroSerializer,
                                IdiomaSerializer)
 
 
-class LivroViewSet(viewsets.ModelViewSet):
+class DefaultsMixin(object):
+    """ Configurações default para autenticação, permissões, filtragem e
+    paginação da view"""
+    authenticaton_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+
+    # permission_classes = (
+    #    permissions.IsAuthenticated,
+    # )
+
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class LivroViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Livro objects """
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
 
 
-class EbookViewSet(viewsets.ModelViewSet):
+class EbookViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Ebook objects """
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
 
 
-class AutorViewSet(viewsets.ModelViewSet):
+class AutorViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Ebook objects """
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
 
 
-class IdiomaViewSet(viewsets.ModelViewSet):
+class IdiomaViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Ebook objects """
     queryset = Idioma.objects.all()
     serializer_class = IdiomaSerializer
