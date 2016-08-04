72843c32fbdd0f27005c1205dcf0e13d576fe54d
Guido <guidopercu@gmail.com>
2016-08-04 00:07:53 -0300

    Creating backbone application and linking it to django urls

--- livros/backend/settings.py
+++ livros/backend/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FRONTEND_DIR = os.path.join(BASE_DIR, 'frontend')
+BACKEND_DIR = os.path.join(BASE_DIR, 'backend')
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(FRONTEND_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(FRONTEND_DIR, 'static/') # TODO Not working
--- livros/backend/urls.py
+++ livros/backend/urls.py
@@ -1,25 +1,16 @@
-"""livros URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from rest_framework.routers import DefaultRouter
 from ebook.views import LivroViewSet, EbookViewSet, AutorViewSet, IdiomaViewSet
 
-router = DefaultRouter()
-router.register(prefix='api/livros', viewset=LivroViewSet)
-router.register(prefix='api/ebooks', viewset=EbookViewSet)
-router.register(prefix='api/autores', viewset=AutorViewSet)
-router.register(prefix='api/idiomas', viewset=IdiomaViewSet)
+from django.views.generic import TemplateView
+from django.conf.urls import url, include
 
-urlpatterns = router.urls
+router = DefaultRouter()
+router.register(prefix='livros', viewset=LivroViewSet)
+router.register(prefix='ebooks', viewset=EbookViewSet)
+router.register(prefix='autores', viewset=AutorViewSet)
+router.register(prefix='idiomas', viewset=IdiomaViewSet)
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^api/', include(router.urls)),
+]
